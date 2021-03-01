4ca3f71e89128e14897d0629c71c030e65da5d21
SMakaiTakori <makait87@icloud.com>
2021-03-01 14:01:56 -0500

    implemented regex to filter out punctuations

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // CHALLENGE 1: LONGEST WORD
 // Return the longest word of a string
-// ex. longestWord('Hi there, my name is Makai') === 'there,'
+// ex. longestWord('Hello there, my name is Sam') === 'there,'
 
-function longestWord(sen) {}
+function longestWord(sen) {
+  //Create filtered array, take out punctuations
+  const wordArray = sen.toLowerCase().match(/[a-z0-9]+/g);
+}
