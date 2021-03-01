1302dd95a654cb76871193b28036b5f5477e79b7
SMakaiTakori <makait87@icloud.com>
2021-03-01 14:12:58 -0500

    filtered array to check each word's length and compare to longest word

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ function longestWord(sen) {
   const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
   //Sort by length
   const sorted = wordArr.sort((a, b) => b.length - a.length);
+  //If multiple words put into an array
+  //Return true if the word length equals the first word's length in the sorted array
+  const longestWordArr = sorted.filter(
+    (word) => word.length === sorted[0].length
+  );
 }
